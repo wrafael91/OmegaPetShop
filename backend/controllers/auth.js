dbf8344154f3199b4bc44ab09dd2adba4af076f3
wrafael91 <wrafaelmp91@gmail.com>
2022-11-05 21:50:39 -0500

    corrigiendo función login

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,7 +1,7 @@
 const { response, request } = require("express")
 const {compareSync} = require("bcryptjs")
 const {sign} = require("jsonwebtoken")
-const {ClienteModel} = require("../models/Cliente")
+const ClienteModel = require("../models/Cliente")
 
 async function login(req = request, res = response){
     const {email, contrasena} = req.body
@@ -9,14 +9,19 @@ async function login(req = request, res = response){
     const cliente = await ClienteModel.findOne({email})
     if(cliente){
         //Si las credenciales son validas
-        if(compareSync(contrasena, cliente.contrasena)){
-            sign({id: cliente.id}, "grupo19-tiend4Mascota$", (err, token)=>{
-                if(err){
-                    res.status(500).send({msg: "Hubo un error"})
-                } else {
-                    res.send({auth: true, token})
-                }
-            })
+        try {
+            if(compareSync(contrasena, cliente.contrasena)){
+                sign({id: cliente.id}, "grupo19-tiend4Mascota$", (err, token)=>{
+                    if(err){
+                        res.status(500).send({msg: "Hubo un error"})
+                    } else {
+                        res.send({auth: true, token})
+                    }
+                })
+            }
+            
+        } catch (error) {
+            res.status(500).send(error)
         }
     } else {
         res.status(400).send({msg:"No existe el cliente"})
