28889486fc9e3e3a1ce898acdda69a0e67a912dc
wrafael91 <wrafaelmp91@gmail.com>
2022-11-05 23:55:44 -0500

    agregando modelo tabla factura

--- backend/models/Factura.js
+++ backend/models/Factura.js
@@ -4,3 +4,15 @@
     -ValorTotal
     -ValorProducto
 */
+const {Schema,model} = require("mongoose")
+
+const facturaSchema = new Schema({
+    idFactura: Number,
+    producto: {type: Schema.Types.ObjectId, ref: "producto"},
+    valorTotal: Number,
+    valorProducto: Number
+})
+
+const FacturaModel = model("factura", facturaSchema)
+
+module.exports = FacturaModel
